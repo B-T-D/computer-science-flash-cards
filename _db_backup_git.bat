d38442aa7bfbb9e830d74e903719de720d8f2170
Ben <bdye71@gmail.com>
2020-08-12 23:55:05 -0400

    Add bat to commit and push the DB as backup, and add automatic backup on stopping local server when server was started by the bat

---
+++ _db_backup_git.bat
@@ -0,0 +1,5 @@
+git add db\cards.db
+git commit -m"Automated DB backup commit"
+git push
+
+PAUSE
---
+++ _run_local_site.bat
@@ -0,0 +1,5 @@
+pipenv run python -m flask run
+
+git add db\cards.db
+git commit -m"Automated DB backup commit"
+git push
